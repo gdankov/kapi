6dd031aab2dd4ac48e43e6c95e0e1dd7b4faa96c
Georgi Dankov <gddankov@gmail.com>
2019-10-30 10:43:23 +0200

    Build & push controller images

--- scripts/build-and-push-images.sh
+++ scripts/build-and-push-images.sh
@@ -5,7 +5,12 @@ readonly ROOTDIR="$BASEDIR/../"
 
 pushd $ROOTDIR
 docker build . -f cmd/bridge/Dockerfile -t "gdankov/kapi-bridge"
+docker build . -f cmd/controller/lrp/Dockerfile -t "gdankov/lrp-controller"
+docker build . -f cmd/controller/staging/Dockerfile -t "gdankov/staging-controller"
+
 docker push "gdankov/kapi-bridge"
+docker push "gdankov/lrp-controller"
+docker push "gdankov/staging-controller"
 pwd
 
 popd
